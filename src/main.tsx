7ac673c5cd6b5759969f8b270706ad9834228d04
Nikita Surcukov <nsurcukov@mail.ru>
2026-02-03 11:30:29 +0400

    feat:Добавил единый механизм уведомлений

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { NotificationProvider } from './app/providers/notification/NotificationProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <NotificationProvider>
+      <App />
+    </NotificationProvider>
   </StrictMode>
 )
