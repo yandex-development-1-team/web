78d1a574759c09c93a01fa0ec8b58637107b7577
Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
2026-02-04 16:54:25 +0300

    feat: добавлены иконки и картинки

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
-
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr({
+      svgrOptions: {
+        svgo: true,
+        svgoConfig: {
+          plugins: [{ name: 'preset-default', params: { overrides: { removeViewBox: false } } }, 'removeDimensions']
+        },
+        titleProp: true
+      }
+    }),
     tailwindcss()
   ],
   resolve: {
