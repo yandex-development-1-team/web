c0453542517f2a3f74f4e7f5a8eb18ac9a8971a1
Nikita Surcukov <nsurcukov@mail.ru>
2026-02-05 19:58:27 +0400

    fix:Исправил инпорт

---
+++ src/app/providers/notification/NotificationProvider.tsx
@@ -0,0 +1,29 @@
+import {  type ReactNode } from 'react'
+import { Toaster, toast } from 'sonner'
+import type { ShowNotificationParams } from './types'
+import { NotificationContext } from './NotificationContext'
+
+const DEFAULT_DURATION = 6000
+
+export const NotificationProvider = ({ children }: { children: ReactNode }) => {
+  const showNotification = ({ message, status, duration = DEFAULT_DURATION }: ShowNotificationParams) => {
+    switch (status) {
+      case 'success':
+        toast.success(message, { duration })
+        break
+      case 'error':
+        toast.error(message, { duration })
+        break
+      case 'info':
+        toast(message, { duration })
+        break
+    }
+  }
+
+  return (
+    <NotificationContext.Provider value={{ showNotification }}>
+      <Toaster richColors position="top-center" />
+      {children}
+    </NotificationContext.Provider>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { NotificationProvider } from './app/providers/NotificationContext/index.ts'
+import { NotificationProvider } from './app/providers/notification'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
