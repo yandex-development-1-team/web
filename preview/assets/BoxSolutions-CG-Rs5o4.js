174d7dcc4d653c6ddd5a64eb4695050889dbeea7
gulnaz-web <gulnaz-web@users.noreply.github.com>
2026-02-05 20:29:46 +0000

    deploy: d80a0250beac24a11d1796dd0c704342e97687c3

---
+++ preview/assets/BoxSolutions-CG-Rs5o4.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-C-ENlJoP.js";const o=()=>n.jsxs(n.Fragment,{children:[n.jsx("h1",{children:"Коробочные решения"}),n.jsx("p",{children:"Страница в разработке..."})]}),s=o;export{s as Component};
