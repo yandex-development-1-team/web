0258376ed16e013cfe6034c8b4a4cf52df9b4be2
Vladimir <vl.jw@yandex.ru>
2026-02-11 00:53:08 +1000

    style: добавлены стили для основных типов кнопок

--- src/components/ui/button/Button.styles.ts
+++ src/components/ui/button/Button.styles.ts
@@ -1,35 +1,27 @@
-import { cva } from "class-variance-authority"
+import { cva } from 'class-variance-authority'
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg text-button font-button transition-all disabled:pointer-events-none disabled:bg-grey-extra-light disabled:text-grey-dark [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-yellow-accent-dark focus-visible:ring-offset-2",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        default:
+          'bg-yellow-accent-light text-black hover:bg-yellow-light active:bg-yellow-accent-dark aria-invalid:bg-red-medium aria-invalid:text-white',
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+          'bg-white ring-1 ring-inset ring-yellow-accent-light text-black hover:ring-yellow-light active:ring-2 active:ring-yellow-accent-dark aria-invalid:ring-2 aria-invalid:ring-red-medium aria-invalid:text-red-medium disabled:bg-transparent disabled:ring-grey-light disabled:text-grey-dark',
+        underline:
+          'bg-transparent text-black underline decoration-yellow-accent-light decoration-1 underline-offset-6 hover:decoration-yellow-light active:decoration-2 active:decoration-yellow-accent-dark disabled:text-grey-dark disabled:decoration-grey-light',
+        ghost: 'bg-transparent hover:text-yellow-accent-dark active:text-yellow-dark disabled:text-grey-light'
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
-        "icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10",
-      },
+        default: 'h-[46px] px-8 py-3',
+        'icon-40': 'size-10 p-2 rounded-lg ',
+        'icon-32': 'size-8 p-1.5 rounded-lg '
+      }
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
+      variant: 'default',
+      size: 'default'
+    }
   }
 )
