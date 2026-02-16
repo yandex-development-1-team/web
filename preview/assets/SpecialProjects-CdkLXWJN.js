9d99d6526f045fd96dcdc7cc5c68b5961d9d1a3b
gulnaz-web <gulnaz-web@users.noreply.github.com>
2026-02-16 12:48:38 +0000

    deploy: 44ad3d0638893d53d46cc2708e11f1789b92b274

---
+++ preview/assets/SpecialProjects-CdkLXWJN.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-mCqT8vsy.js";const e=()=>r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Управление спецпроектами"}),r.jsx("p",{children:"Страница в разработке..."})]}),s=e;export{s as Component};
