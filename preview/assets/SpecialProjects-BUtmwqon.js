2ed4a0ebe52a3344e0a96d4b595ee51dfd33c61a
Gittenhuben <Gittenhuben@users.noreply.github.com>
2026-02-16 13:10:58 +0000

    deploy: 2e670fcfe41eb07157871a3230dea34f6debfc4e

---
+++ preview/assets/SpecialProjects-BUtmwqon.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DVTBt0cW.js";const e=()=>r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Управление спецпроектами"}),r.jsx("p",{children:"Страница в разработке..."})]}),s=e;export{s as Component};
