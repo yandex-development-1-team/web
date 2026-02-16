46e0d3a909d90b63fccb3ed62a3ebf79005d6fc1
Andrey <ae55@yandex.ru>
2026-02-16 13:33:00 +0300

    fix naming 2

--- src/components/ui/button/Button.types.ts
+++
@@ -1,8 +0,0 @@
-export interface ButtonProps {
-  label?: string
-  onClick?: () => void
-  style?: 'primary' | 'secondary'
-  type?: 'button' | 'submit' | 'reset'
-  className?: string
-  children?: React.ReactNode
-}
