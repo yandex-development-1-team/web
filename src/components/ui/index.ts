499e0224a22788f15ab4596654e1b88ba1df4c07
Diliara <novoobrashennaya@yandex.ru>
2026-02-17 19:38:27 +0300

    fix: импорты иконок в BoxButton, экспорты компонентов из ui

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,19 @@
 export { ToggleButton } from './ToggleButton'
 export { Switch } from './Switch'
 export { Button } from './button'
-export { BoxButton } from './boxButton'
+export { BoxButton } from './BoxButton'
 export { Input } from './input'
+export { IconInput } from './IconInput'
+export { Textarea } from './Textarea'
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue
+} from './Select'
