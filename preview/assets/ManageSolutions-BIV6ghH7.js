7e6c6d9929fd3f088b7ed2ce94c09720a0d220f9
di120 <di120@users.noreply.github.com>
2026-02-19 15:03:20 +0000

    deploy: fea44cfe366124f5c3aff0e7bbdbfdfeebcb6712

---
+++ preview/assets/ManageSolutions-BIV6ghH7.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-DsJYhStj.js";const o=()=>n.jsxs(n.Fragment,{children:[n.jsx("h1",{children:"Управление коробками"}),n.jsx("p",{children:"Страница в разработке..."})]}),s=o;export{s as Component};
