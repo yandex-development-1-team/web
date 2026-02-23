9b6551abeac0a0d4a79d6324082e0078deb34a9c
Gittenhuben <Gittenhuben@users.noreply.github.com>
2026-02-23 00:11:58 +0000

    deploy: 540a4378e7acee45ccb877329fc422160fe9a99e

---
+++ preview/assets/BoxSolutions-sbWCRg3q.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-CPSnIM8F.js";const o=()=>n.jsxs(n.Fragment,{children:[n.jsx("h1",{children:"Коробочные решения"}),n.jsx("p",{children:"Страница в разработке..."})]}),s=o;export{s as Component};
---
+++ preview/assets/DataExport-DaH0IGoa.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CPSnIM8F.js";const t=()=>r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Экспортированные файлы"}),r.jsx("p",{children:"Страница в разработке..."})]}),o=t;export{o as Component};
