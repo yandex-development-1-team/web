d879dea4cc39460518d44bc74e74eb4214b84009
Kadokan69 <Kadokan69@users.noreply.github.com>
2026-02-25 19:00:54 +0000

    deploy: b34f96d676882f72216069876322b95f8c04ea36

---
+++ preview/assets/SpecialProjects-ttdWs1LD.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CGBDcjST.js";const e=()=>r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Управление спецпроектами"}),r.jsx("p",{children:"Страница в разработке..."})]}),s=e;export{s as Component};
