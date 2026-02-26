05cac45d50f208abf37b9e86984dcf6359e8e5ec
Nikita Surtcukov <nsurcukov@mail.ru>
2026-02-26 15:30:58 +0400

    fix:Добавил базовую валидацию, вынес в отдельный файл форму, поправил стили

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,52 +1,14 @@
-import { useState } from 'react'
 import loginImage from '@/assets/images/Login_Image.png'
-
-import EventLogo from '@/assets/icons/Event.svg?react'
-import EyeButton from '@/assets/icons/Eye.svg?react'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
+import { EventIcon } from '@/assets/icons'
+import { LoginForm } from './LoginForm'
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false)
   return (
     <div className="flex max-w-[1376px] w-full h-screen overflow-hidden py-[20px] px-[32px] gap-[20px] ">
       <div className="flex flex-col w-[445px] min-w-[445px] h-full pt-[20px] pr-[66px] pl-[67px] rounded-[8px] bg-white">
-        <EventLogo className="h-[51px] w-[96px]" />
+        <EventIcon className="h-[51px] w-[96px]" />
         <div className="flex flex-col justify-center flex-1 ">
           <h2 className="mb-[32px] font-bold text-[32px] text-left">Вход</h2>
-          <form action="">
-            <Input
-              placeholder="Логин"
-              required
-              className="mb-[20px] rounded-[8px] p-[12px]  min-h-[46px] border-grey-light italic text-small"
-            />
-            <div className="relative mb-[4px]">
-              <Input
-                placeholder="Введите пароль"
-                type={showPassword ? 'text' : 'password'}
-                required
-                minLength={8}
-                className="rounded-[12px] min-h-[46px] pr-[40px] italic font-[14px] border-grey-light"
-              />
-
-              <button
-                type="button"
-                onClick={() => setShowPassword(show => !show)}
-                className="absolute right-[12px] top-1/2 -translate-y-1/2"
-              >
-                <EyeButton />
-              </button>
-            </div>
-
-            <div className="mb-[40px]">
-              <a href="#!" className="text-left text-text-grey-dark">
-                Забыли пароль?
-              </a>
-            </div>
-
-            <Button className="w-full min-h-[46px] bg-yellow-accent-light text-text text-button font-semibold rounded-[12px] py-[12px]">
-              Войти
-            </Button>
-          </form>
+          <LoginForm />
         </div>
       </div>
 
