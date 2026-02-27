dc452759d197e0c64b52fa14ff75dde6fab9a7d2
Andrey <ae55@yandex.ru>
2026-02-27 05:13:06 +0300

    fix: api path

--- src/app/providers/axios/axiosInstance.ts
+++ src/app/providers/axios/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { AxiosRequestConfig } from 'axios'
 
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api/v1',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
