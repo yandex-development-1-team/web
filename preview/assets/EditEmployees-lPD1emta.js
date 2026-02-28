9091126c70d082bc49986e352927181335c47b48
gulnaz-web <gulnaz-web@users.noreply.github.com>
2026-02-28 13:56:45 +0000

    deploy: 72ede2948727eb48f9654d6a2f9783a97276c6e5

---
+++ preview/assets/EditEmployees-lPD1emta.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CExNIuKr.js";const n=()=>e.jsxs(e.Fragment,{children:[e.jsx("h1",{children:"Редактировать сотрудника"}),e.jsx("p",{children:"Страница в разработке..."})]}),s=n;export{s as Component};
---
+++ preview/assets/ViewEmployees-KkU_5cyH.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CExNIuKr.js";const n=()=>e.jsxs(e.Fragment,{children:[e.jsx("h1",{children:"Карточка сотрудника"}),e.jsx("p",{children:"Страница в разработке..."})]}),s=n;export{s as Component};
