18dad5a80e2128d1c64c263bc702ad1cd9ceaf09
Gittenhuben <Gittenhuben@users.noreply.github.com>
2026-03-05 12:03:30 +0000

    deploy: 943fced29eda9a67eb9461a45621d40849a2802d

---
+++ preview/assets/SpecialProjects-ThxTgXbr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BY8NgYhX.js";const e=()=>r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:"Управление спецпроектами"}),r.jsx("p",{children:"Страница в разработке..."})]}),s=e;export{s as Component};
