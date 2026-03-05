8e336667265a241fbca0ecd177f937b4a72dee60
Maksim Dahovnik <dahovnik@gmail.com>
2026-03-05 22:56:40 +0200

    refactor: add useEmployee hook

--- src/pages/Employees/id/ViewEmployees/ViewEmployees.tsx
+++ src/pages/Employees/id/ViewEmployees/ViewEmployees.tsx
@@ -1,42 +1,21 @@
 import { ROUTES } from '@/app/router'
-import { Button, Card, Loader } from '@/components/ui'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { href, useNavigate, useParams } from 'react-router-dom'
-import { getEmployeeById } from '../../api/getEmployeeById'
-import { updateEmployeeStatus } from '../../api/updateEmployeeStatus'
-import type { StatusType } from '../../employees.types'
+import { Card, Loader } from '@/components/ui'
+import { cn } from '@/lib/utils.clsx'
+import { href, Link, useParams } from 'react-router-dom'
+import { useEmployee } from './hooks/useEmployee'
 import { Content } from './ui/Content'
 import { EmployeeActions } from './ui/EmployeeActions'
 import { EmployeeDetails } from './ui/EmployeeDetails'
 
 const ViewEmployees = () => {
   const { employeeId = '' } = useParams<{ employeeId: string }>()
-  const navigate = useNavigate()
+  const { employee, isPending, error, toggleStatus } = useEmployee(employeeId)
 
-  const { data, isPending, isError } = useQuery({
-    queryKey: ['employee', employeeId],
-    queryFn: () => {
-      if (!employeeId) throw new Error('ID is required')
-      return getEmployeeById(employeeId)
-    }
-  })
-
-  const queryClient = useQueryClient()
-  const mutation = useMutation({
-    mutationFn: (status: StatusType) => {
-      if (!employeeId) throw new Error('ID is required')
-      return updateEmployeeStatus({ employeeId, status })
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['employee', employeeId] })
-    }
-  })
-
-  if (isError) return <div className="text-h4 text-text-grey-dark">Ошибка при получении данных</div>
+  if (error) return <div className="text-h4 text-text-grey-dark">Ошибка при получении данных</div>
   if (isPending) return <Loader />
-  if (!data) return <div className="text-h4 text-text-grey-dark">Сотрудник не найден</div>
+  if (!employee) return <div className="text-h4 text-text-grey-dark">Сотрудник не найден</div>
 
-  const { avatar, status, ...restData } = data
+  const { avatar, status, ...restData } = employee
 
   return (
     <>
@@ -44,21 +23,21 @@ const ViewEmployees = () => {
         <h1 className=" text-text-black-dark text-h2">Карточка сотрудника</h1>
       </Card>
       <Content className="mt-5 grid grid-cols-[380px_1fr] gap-5 max-h-max">
-        <EmployeeActions
-          avatar={avatar}
-          status={status}
-          onStatusChange={() => mutation.mutate(status === 'active' ? 'inactive' : 'active')}
-        />
+        <EmployeeActions avatar={avatar} status={status} onStatusChange={toggleStatus} /> //TODO: прокинуть
+        isUpdatingStatus для блокировки кнопки
         <EmployeeDetails profile={restData} />
       </Content>
-      <Button
-        variant={'default'}
-        size={'default'}
-        className="w-46 self-end mt-5"
-        onClick={() => navigate(href(ROUTES.employeesEdit, { employeeId }))}
+      <Link
+        className={cn(
+          'w-46 self-end mt-5',
+          'bg-yellow-accent-light text-black text-button',
+          'hover:bg-yellow-light active:bg-yellow-accent-dark aria-invalid:bg-red-medium aria-invalid:text-white',
+          'h-11.5 px-8 py-3 rounded-lg transition-colors duration-300 ease-in-out'
+        )}
+        to={href(ROUTES.employeesEdit, { employeeId })}
       >
         Редактировать
-      </Button>
+      </Link>
     </>
   )
 }
