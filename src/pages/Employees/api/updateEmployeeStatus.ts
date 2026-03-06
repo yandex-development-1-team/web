8eb50072a8e3e19d602301d1c1def5b98efe8fee
Maksim Dahovnik <dahovnik@gmail.com>
2026-03-06 10:56:18 +0200

    refactor(api): remove delay from updateEmployeeStatus

--- src/pages/Employees/api/updateEmployeeStatus.ts
+++ src/pages/Employees/api/updateEmployeeStatus.ts
@@ -3,6 +3,5 @@ import type { StatusType } from '../employees.types'
 
 export const updateEmployeeStatus = async ({ employeeId, status }: { employeeId: string; status: StatusType }) => {
   const response = await api.patch(`/emloyee/${employeeId}`, status)
-  await new Promise(res => setTimeout(res, 2000))
   return response.data
 }
