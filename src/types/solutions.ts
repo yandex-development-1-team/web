5a32c60d1e617317849bccf4fcf8198cb42146da
Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
2026-03-12 02:49:36 +0300

    feat: добавлена загрузка изображений

--- src/types/solutions.ts
+++ src/types/solutions.ts
@@ -2,14 +2,15 @@ export interface BoxData {
   id: number
   name: string
   isActive: boolean
-  date?: string // ISO формат "2024-03-10"
-  startTime?: string // "00:00"
-  endTime?: string // "00:00"
+  date: string | undefined // ISO формат "2024-03-10"
+  startTime: string | undefined // "00:00"
+  endTime: string | undefined // "00:00"
   location: string
   description: string
   rules: string
   cost: string
   organizer: string
+  image?: string
 }
 
 export interface IProject {
