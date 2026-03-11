5243799240a5d738583d10a555d31686a193a50b
Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
2026-03-11 03:30:12 +0300

    feat: организован сбор данных из формы, модалка работает на странице Управление коробками

--- src/components/BoxSolutionModal/BoxSolutionModal.type.ts
+++ src/components/BoxSolutionModal/BoxSolutionModal.type.ts
@@ -1,7 +1,25 @@
+import type { TimeRange } from '@/lib/utils.time'
+import type { BoxData } from '@/types/solutions'
+
 export type ModalAction = 'create' | 'edit'
 
 export type BoxSolutionModalType = {
   isOpen: boolean
   onClose: () => void
   action: ModalAction
+  boxData?: BoxData
+  onSave: (data: Omit<BoxData, 'id'>) => void
+}
+
+export type BoxSolutionFormData = {
+  name: string
+  isActive: boolean
+  date?: Date
+  timeRange?: TimeRange
+  location: string
+  description: string
+  rules: string
+  cost: string
+  organizer: string
+  image?: File
 }
