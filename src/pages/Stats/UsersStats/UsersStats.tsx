4485179f213a07b47157a0af251a08f81bea1df1
Dinar Samigullin <Dinarik6598@yandex.ru>
2026-03-14 18:40:25 +0500

    feat: task_96

--- src/pages/Stats/UsersStats/UsersStats.tsx
+++ src/pages/Stats/UsersStats/UsersStats.tsx
@@ -1,8 +1,32 @@
+import { indicators } from './usersStatsData'
+import { mockUsersIndicatorsValues } from '@/mockData/mockUsersStatsPageData'
+
 const UsersStats = () => {
   return (
     <>
-      <h1>Аналитика пользователей</h1>
-      <p>Страница в разработке...</p>
+      <div className="bg-white text-text-black-dark px-[20px] pb-[20px] rounded-[8px]">
+        <h2 className="text-h2 py-[18px_13px]">Аналитика пользователей</h2>
+        <h4 className="text-h4sb pb-[14px]">Сводка</h4>
+        <div className="flex text-text gap-[20px]">
+          {indicators.map(indicator => (
+            <div className="flex-1" key={indicator.id}>
+              <p className="text-xxs pb-[7px]">{indicator.name}</p>
+              <div
+                className={`
+                border-1 border-grey-light rounded-[8px] text-indicator text-center py-[30px]
+                ${indicator.warningColor && 'text-text-error'}
+              `}
+              >
+                {mockUsersIndicatorsValues.find(el => el.id === indicator.id)?.value || 0}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="grid grid-cols gap-[20px] bg-white text-text-black-dark px-[20px] pb-[20px] mt-[20px] rounded-[8px]">
+        <div></div>
+        <div></div>
+      </div>
     </>
   )
 }
