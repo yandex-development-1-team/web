1cf845d52e3dfbebe930759891f049ea22fc0131
Maksim Dahovnik <dahovnik@gmail.com>
2026-03-15 18:33:26 +0200

    chore: remove mockData

--- src/pages/Stats/Attendance/Attendance.tsx
+++ src/pages/Stats/Attendance/Attendance.tsx
@@ -1,32 +1,8 @@
-import { SummaryCardsList } from '@/components/SummaryCarsdList/SummaryCardsList'
-import type { SummaryCardType } from '@/components/ui/SummaryCard/SummaryCard.types'
-
-const mockSummaryCards: SummaryCardType[] = [
-  {
-    title: 'Средняя посещаемость',
-    value: 87
-  },
-  {
-    title: 'Часы в работе',
-    value: 164
-  },
-  {
-    title: 'Завершено задач',
-    value: 4
-  },
-  {
-    title: 'Эффективность',
-    value: 9.4
-  }
-]
-
 const Attendance = () => {
   return (
     <>
       <h1>Средняя посещаемость на коробку</h1>
       <p>Страница в разработке...</p>
-
-      <SummaryCardsList cards={mockSummaryCards}></SummaryCardsList>
     </>
   )
 }
