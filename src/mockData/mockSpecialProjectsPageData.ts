e9c9c3c51146cfb422c5bf0880f0b401f72445f4
Andrey <ae55@yandex.ru>
2026-03-22 22:12:24 +0300

    fix url input

--- src/mockData/mockSpecialProjectsPageData.ts
+++ src/mockData/mockSpecialProjectsPageData.ts
@@ -6,3 +6,5 @@ export const mockProjects: IProject[] = [
   { id: 1, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false },
   { id: 2, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false }
 ]
+
+export const mockUrl = 'https://forms.yandex.ru/u/65f2a9c3e4b0a123456789ab/'
