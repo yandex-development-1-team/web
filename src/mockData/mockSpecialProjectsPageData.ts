943992443932b48df8398e158c29f22329e7c66e
Andrey <ae55@yandex.ru>
2026-03-28 02:51:21 +0300

    fix: adaptive

--- src/mockData/mockSpecialProjectsPageData.ts
+++ src/mockData/mockSpecialProjectsPageData.ts
@@ -4,7 +4,11 @@ import mockGallery from './mock_gallery.jpg'
 export const mockProjects: IProject[] = [
   { id: 0, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: true },
   { id: 1, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false },
-  { id: 2, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false }
+  { id: 2, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false },
+  { id: 3, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false },
+  { id: 4, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false },
+  { id: 5, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false },
+  { id: 6, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, status: false }
 ]
 
 export const mockUrl = 'https://forms.yandex.ru/u/65f2a9c3e4b0a123456789ab/'
