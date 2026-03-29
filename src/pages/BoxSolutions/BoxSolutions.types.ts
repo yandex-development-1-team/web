4035a255f0bebb3ce6c8d1a3e7d69736ec97f45f
Maksim Dahovnik <dahovnik@gmail.com>
2026-03-29 14:40:46 +0200

    refactor: make parseQueryParam generic

--- src/pages/BoxSolutions/BoxSolutions.types.ts
+++ src/pages/BoxSolutions/BoxSolutions.types.ts
@@ -1,3 +1,5 @@
+import z from 'zod'
+
 export type BoxStatus = 'active' | 'disable'
 
 export interface IBox {
@@ -25,3 +27,18 @@ export interface IPagination {
 }
 
 export type ModalState = { type: 'delete' | 'edit'; id: number } | { type: 'create'; id: null } | null
+
+const boxStatusField = ['active', 'disable'] as const
+const boxSortField = ['name', 'created_at', 'updated_at'] as const
+const boxOrderField = ['asc', 'desc'] as const
+
+export const paramsSchema = z.object({
+  limit: z.coerce.number().min(1).max(20).default(6),
+  offset: z.coerce.number().min(0).default(0),
+  status: z.enum(boxStatusField).optional(),
+  search: z.string().optional(),
+  sort: z.enum(boxSortField).optional(),
+  order: z.enum(boxOrderField).optional()
+})
+
+export type BoxSolutionsSearchParamsType = z.infer<typeof paramsSchema>
