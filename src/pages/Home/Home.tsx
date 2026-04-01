653dcdc2468a5a97a663797b3b47560663d8d609
Никита <164332229+nsurcukov@users.noreply.github.com>
2026-04-01 23:54:46 +0400

    Feature/task 109 main page (#152)
    * feat/сделать главную страницу
    
    * fix/поправил стили, поправил верстку
    
    * fix/поправить названия цветов на странице, перенести моковые данные в другие файлы, вынести в глобальные переменные цвета статуса
    
    * fix/перенес типы в одно место, поправил стили
    
    * fix
    
    * feat: добавлен TimeRangeInput
    
    * fix: перенесла компоненты
    
    * Feature/update datatable and pagination (#143)
    
    * fix: Убрал пагинацию.
    
    * add Pagination component
    
    * fix
    
    * fix
    
    * refactor usePaginationNav
    
    * add DataTable pagination prop
    
    ---------
    
    Co-authored-by: Andrey Kelorev <kelorev.andrey@yandex.ru>
    Co-authored-by: Maksim Dahovnik <dahovnik@gmail.com>
    
    * feat(ui): add SummaryCard and SummaryCardsList (#127)
    
    * feat(ui): add SummaryCard and SummaryCardsList
    
    * chore: remove mockData
    
    * fix(type): add card status
    
    * fix:поправить адаптив, исправить dropdown, убрать dropdown у кнопок в таблице
    
    * fix:убрал лишний стейт
    
    * fix: поправил верстку
    
    ---------
    
    Co-authored-by: Nikita Surtcukov <nsurcukov@mail.ru>
    Co-authored-by: Andrey <ae55@yandex.ru>
    Co-authored-by: Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
    Co-authored-by: Gittenhuben <138328162+Gittenhuben@users.noreply.github.com>
    Co-authored-by: Andrey Kelorev <kelorev.andrey@yandex.ru>
    Co-authored-by: Maksim Dahovnik <dahovnik@gmail.com>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,92 @@
+import { BoxButton, Button, DataTable } from '@/components/ui'
+import { Application2 } from '@/assets/icons'
+import { useState } from 'react'
+import FilterDropdown from './ui/FilterDropdown'
+import { headerTableData } from './homePageData'
+import { bookingRequestsMock } from '@/mockData/bookingRequestsMock'
+
 const Home = () => {
+  const [statusFilter, setStatusFilter] = useState('all')
+  const data = bookingRequestsMock
+
+  const countQueue = data.filter(item => item.status === 'queue').length
+  const countInProgress = data.filter(item => item.status === 'progress').length
+  const countDone = data.filter(item => item.status === 'done').length
+
+  const stats = [
+    { title: 'Новые заявки', value: countQueue },
+    { title: 'Заявки в работе', value: countInProgress }
+  ]
+
+  const handleBoxCreate = () => {}
+
+  const handleSpecProjectCreate = () => {}
+
+  const filteredData = statusFilter === 'all' ? data : data.filter(item => item.status === statusFilter)
+
   return (
-    <>
-      <h1>Главная страница</h1>
-      <p>Страница в разработке...</p>
-    </>
+    <div className="flex flex-col gap-[20px]">
+      <h2 className="text-h2">Главная страница</h2>
+
+      <div className="flex gap-[20px] items-end">
+        {stats.map((stat, index) => (
+          <div key={stat.title} className="flex flex-col flex-1">
+            <span className="text-h5 mb-[8px] text-text-grey-dark">{stat.title}</span>
+
+            <Button
+              className={`h-[92px] text-[48px] font-bold ${index === 1 ? 'bg-white border border-grey-light' : ''}`}
+            >
+              {stat.value}
+            </Button>
+          </div>
+        ))}
+
+        <BoxButton onClick={handleBoxCreate} icon="box" className="max-w-[407px]">
+          Создать коробку
+        </BoxButton>
+      </div>
+
+      <div className="flex gap-[20px] h-[92px]">
+        <div className="flex justify-between items-center flex-1 border bg-white  border-grey-light  rounded-[8px] px-[43px] min-w-[468px] ">
+          <div className="flex items-center lg:gap-[12px] gap-[3px]">
+            <Application2 width={32} height={32} />
+            <span className="text-h5 font-semibold">Мои заявки</span>
+          </div>
+
+          <div className="flex lg:gap-[20px] gap-[10px]">
+            <div className="flex flex-col justify-center items-center min-w-[100px] lg:gap-[4px] gap-[2px] xl:min-w-[185px] text-text-grey-dark">
+              <span className="text-h5 ">В работе: </span>
+              <span className="text-h3 font-bold">{countInProgress}</span>
+            </div>
+
+            <div className="flex flex-col justify-center items-center min-w-[100px] gap-[4px] max-w-[185px] text-text-grey-dark ">
+              <span className="text-h5 ">Обработаны: </span>
+              <span className="text-h3 font-bold">{countDone}</span>
+            </div>
+          </div>
+        </div>
+
+        <BoxButton onClick={handleSpecProjectCreate} icon="special_projects" className="max-w-[407px]">
+          Создать спецпроект
+        </BoxButton>
+      </div>
+
+      <div>
+        <h3 className="mb-[12px] text-h3">Заявки на бронирование</h3>
+
+        <div className="min-w-[320px]">
+          <div className="flex flex-col gap-[4px] mb-[19px]">
+            <span className="text-xxs text-text-grey-dark">Фильтр</span>
+            <FilterDropdown
+              value={statusFilter}
+              onChange={setStatusFilter}
+              className="text-text-grey-light text-small italic px-[6px] py-[12px] border border-grey-light rounded-[8px] pl-[12px] xl:min-w-[494px]  md:min-w-[320px] bg-white"
+            />
+          </div>
+          <DataTable idKey="id" data={filteredData} enableLoadMore columns={headerTableData} />
+        </div>
+      </div>
+    </div>
   )
 }
-
 export const Component = Home
