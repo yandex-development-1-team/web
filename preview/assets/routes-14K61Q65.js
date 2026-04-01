a753fd0571f46fe158df94c88a04b5caeb38dc94
Gittenhuben <Gittenhuben@users.noreply.github.com>
2026-04-01 00:00:32 +0000

    deploy: bdb2792288480b48f508c6382f6fae73b6e71a49

---
+++ preview/assets/routes-14K61Q65.js
@@ -0,0 +1,1 @@
+const e="/export",s={items:"/items",analytics:{boxes:"/analytics/boxes",export:"/analytics/export"},export:{byId:t=>`${e}/${t}`,deleteById:t=>`${e}/${t}`,byPath:t=>`${e}/${t}`}};export{s as A};
