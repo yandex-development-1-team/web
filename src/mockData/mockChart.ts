e37eddfde4b14a4adf6c97efe85e0f1e67d4d09c
gulnaz.web <73237809+gulnaz-web@users.noreply.github.com>
2026-04-07 01:23:33 +0300

    Feature/task 150 component graph (#178)
    * feat: Реализация компонента График
    
    * fix: Убрал избыточные проверки. Скорректироваол mock данные
    
    * fix: Проверка на пустой массив periods
    
    * fix dependencies
    
    ---------
    
    Co-authored-by: Andrey Kelorev <kelorev.andrey@yandex.ru>
    Co-authored-by: Andrey <ae55@yandex.ru>
    Co-authored-by: Gittenhuben <138328162+Gittenhuben@users.noreply.github.com>

---
+++ src/mockData/mockChart.ts
@@ -0,0 +1,60 @@
+export const MOCK_CHART = {
+  periods: [
+    {
+      id: 1,
+      name: '2024-03-01-2024-03-05',
+      data: [
+        { date: '2024-03-01', visitors: 245 },
+        { date: '2024-03-02', visitors: 267 },
+        { date: '2024-03-03', visitors: 289 },
+        { date: '2024-03-04', visitors: 312 },
+        { date: '2024-03-05', visitors: 298 }
+      ]
+    },
+    {
+      id: 2,
+      name: '2025-03-01-2025-03-05',
+      data: [
+        { date: '2025-03-01', visitors: 345 },
+        { date: '2025-03-02', visitors: 367 },
+        { date: '2025-03-03', visitors: 389 },
+        { date: '2025-03-04', visitors: 412 },
+        { date: '2025-03-05', visitors: 398 }
+      ]
+    },
+    {
+      id: 3,
+      name: '2026-03-01-2026-03-05',
+      data: [
+        { date: '2026-03-01', visitors: 445 },
+        { date: '2026-03-02', visitors: 467 },
+        { date: '2026-03-03', visitors: 489 },
+        { date: '2026-03-04', visitors: 512 },
+        { date: '2026-03-05', visitors: 498 }
+      ]
+    },
+    {
+      id: 4,
+      name: '2023-03-01-2023-03-05',
+      data: [
+        { date: '2025-03-01', visitors: 425 },
+        { date: '2025-03-02', visitors: 457 },
+        { date: '2025-03-03', visitors: 479 },
+        { date: '2025-03-04', visitors: 542 },
+        { date: '2025-03-05', visitors: 538 }
+      ]
+    },
+    {
+      id: 5,
+      name: '2024-04-02-2024-04-10',
+      data: [
+        { date: '2024-04-02', visitors: 398 },
+        { date: '2024-04-04', visitors: 434 },
+        { date: '2024-04-05', visitors: 450 },
+        { date: '2024-04-07', visitors: 478 },
+        { date: '2024-04-08', visitors: 502 },
+        { date: '2024-04-10', visitors: 489 }
+      ]
+    }
+  ]
+}
