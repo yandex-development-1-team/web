3e3b7834804a1b6b73152b76e099a6c3d98da9ad
Andrey <ae55@yandex.ru>
2026-04-10 00:19:03 +0300

    ManageSolutions page modals integration

--- src/mockData/mockManageSolutionsPageData.ts
+++ src/mockData/mockManageSolutionsPageData.ts
@@ -1,5 +1,6 @@
 import type { BoxData, IProject } from '@/types/solutions'
 import type { IIndicatorValues } from '@/types/indicators'
+import mockGallery from './mock_gallery.jpg'
 
 export const mockIndicatorsValues: IIndicatorValues[] = [
   { id: 0, value: 5 },
@@ -94,9 +95,9 @@ export const mockBoxes: BoxData[] = [
   }
 ]
 
-export const mockProjects: Pick<IProject, 'id' | 'title'>[] = [
-  { id: 0, title: 'Название спецпроекта' },
-  { id: 1, title: 'Название спецпроекта' },
-  { id: 2, title: 'Название спецпроекта' },
-  { id: 3, title: 'Название спецпроекта' }
+export const mockProjects: IProject[] = [
+  { id: 0, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, isActive: true },
+  { id: 1, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, isActive: false },
+  { id: 2, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, isActive: false },
+  { id: 3, title: 'Большой театр', description: 'Театральная площадь, 1', image: mockGallery, isActive: false }
 ]
