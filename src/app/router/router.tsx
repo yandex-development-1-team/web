b683b7fafd18a10f1982ebb20b4af72f78377b5a
Andrey <ae55@yandex.ru>
2026-04-10 06:13:03 +0300

    fix

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -25,103 +25,108 @@ export const router = createBrowserRouter(
           lazy: () => import('@/pages/Login/Login')
         },
         {
-          element: <App />,
+          element: <ProtectedRoute />,
           children: [
             {
-              path: ROUTES.home,
-              lazy: () => import('@/pages/Home/Home')
-            },
-            {
-              element: <ProtectedRoute requiredPermission={PERMISSIONS.boxesView} />,
+              element: <App />,
               children: [
                 {
-                  path: ROUTES.boxSolutions,
-                  lazy: () => import('@/pages/BoxSolutions/BoxSolutions')
+                  path: ROUTES.home,
+                  lazy: () => import('@/pages/Home/Home')
+                },
+                {
+                  element: <ProtectedRoute requiredPermission={PERMISSIONS.boxesView} />,
+                  children: [
+                    {
+                      path: ROUTES.boxSolutions,
+                      lazy: () => import('@/pages/BoxSolutions/BoxSolutions')
+                    }
+                  ]
+                },
+                {
+                  element: <ProtectedRoute requiredPermission={PERMISSIONS.specprojectsView} />,
+                  children: [
+                    {
+                      path: ROUTES.specialProjects,
+                      lazy: () => import('@/pages/SpecialProjects/SpecialProjects')
+                    }
+                  ]
+                },
+                {
+                  element: <ProtectedRoute requiredPermission={PERMISSIONS.applicationsView} />,
+                  children: [
+                    {
+                      path: ROUTES.applications,
+                      lazy: () => import('@/pages/Applications/Applications')
+                    }
+                  ]
+                },
+                {
+                  path: ROUTES.resources,
+                  lazy: () => import('@/pages/Resources/Resources')
+                },
+                {
+                  element: <ProtectedRoute requiredRole="admin" />,
+                  children: [
+                    {
+                      path: ROUTES.stats,
+                      lazy: () => import('@/pages/Stats/Stats')
+                    },
+                    {
+                      path: ROUTES.statsAttendance,
+                      lazy: () => import('@/pages/Stats/Attendance/Attendance')
+                    },
+                    {
+                      path: ROUTES.statsApplications,
+                      lazy: () => import('@/pages/Stats/ApplicationsStats/ApplicationStats')
+                    },
+                    {
+                      path: ROUTES.statsPopularity,
+                      lazy: () => import('@/pages/Stats/Popularity/Popularity')
+                    },
+                    {
+                      path: ROUTES.statsDataExport,
+                      lazy: () => import('@/pages/Stats/DataExport/DataExport')
+                    },
+                    {
+                      path: ROUTES.statsUsers,
+                      lazy: () => import('@/pages/Stats/UsersStats/UsersStats')
+                    },
+                    {
+                      path: ROUTES.manageSolutions,
+                      lazy: () => import('@/pages/ManageSolutions/ManageSolutions')
+                    },
+                    {
+                      path: ROUTES.schedule,
+                      lazy: () => import('@/pages/Schedule/Schedule')
+                    },
+                    {
+                      path: ROUTES.employees,
+                      lazy: () => import('@/pages/Employees/Employees')
+                    },
+                    {
+                      path: ROUTES.employeesCreate,
+                      lazy: () => import('@/pages/Employees/CreateEmployees/CreateEmployees')
+                    },
+                    {
+                      path: ROUTES.employeesEdit,
+                      lazy: () => import('@/pages/Employees/id/EditEmployees/EditEmployees')
+                    },
+                    {
+                      path: ROUTES.employeesView,
+                      lazy: () => import('@/pages/Employees/id/ViewEmployees/ViewEmployees')
+                    },
+                    {
+                      path: ROUTES.settings,
+                      lazy: () => import('@/pages/Settings/Settings')
+                    }
+                  ]
+                },
+                {
+                  path: '*',
+                  lazy: () => import('@/pages/NotFound/NotFound')
                 }
               ]
-            },
-            {
-              element: <ProtectedRoute requiredPermission={PERMISSIONS.specprojectsView} />,
-              children: [
-                {
-                  path: ROUTES.specialProjects,
-                  lazy: () => import('@/pages/SpecialProjects/SpecialProjects')
-                }
-              ]
-            },
-            {
-              element: <ProtectedRoute requiredPermission={PERMISSIONS.applicationsView} />,
-              children: [
-                {
-                  path: ROUTES.applications,
-                  lazy: () => import('@/pages/Applications/Applications')
-                }
-              ]
-            },
-            {
-              path: ROUTES.resources,
-              lazy: () => import('@/pages/Resources/Resources')
-            },
-            {
-              element: <ProtectedRoute requiredRole="admin" />,
-              children: [
-                {
-                  path: ROUTES.stats,
-                  lazy: () => import('@/pages/Stats/Stats')
-                },
-                {
-                  path: ROUTES.statsAttendance,
-                  lazy: () => import('@/pages/Stats/Attendance/Attendance')
-                },
-                {
-                  path: ROUTES.statsApplications,
-                  lazy: () => import('@/pages/Stats/ApplicationsStats/ApplicationStats')
-                },
-                {
-                  path: ROUTES.statsPopularity,
-                  lazy: () => import('@/pages/Stats/Popularity/Popularity')
-                },
-                {
-                  path: ROUTES.statsDataExport,
-                  lazy: () => import('@/pages/Stats/DataExport/DataExport')
-                },
-                {
-                  path: ROUTES.statsUsers,
-                  lazy: () => import('@/pages/Stats/UsersStats/UsersStats')
-                },
-                {
-                  path: ROUTES.manageSolutions,
-                  lazy: () => import('@/pages/ManageSolutions/ManageSolutions')
-                },
-                {
-                  path: ROUTES.schedule,
-                  lazy: () => import('@/pages/Schedule/Schedule')
-                },
-                {
-                  path: ROUTES.employees,
-                  lazy: () => import('@/pages/Employees/Employees')
-                },
-                {
-                  path: ROUTES.employeesCreate,
-                  lazy: () => import('@/pages/Employees/CreateEmployees/CreateEmployees')
-                },
-                {
-                  path: ROUTES.employeesEdit,
-                  lazy: () => import('@/pages/Employees/id/EditEmployees/EditEmployees')
-                },
-                {
-                  path: ROUTES.employeesView,
-                  lazy: () => import('@/pages/Employees/id/ViewEmployees/ViewEmployees')
-                },
-                {
-                  path: ROUTES.settings,
-                  lazy: () => import('@/pages/Settings/Settings')
-                }
-              ]
-            },
-            {
-              path: '*',
-              lazy: () => import('@/pages/NotFound/NotFound')
             }
           ]
         }
