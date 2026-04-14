75c74ef4fc515718ee393e155706e2b8bcccf522
Gittenhuben <Gittenhuben@users.noreply.github.com>
2026-04-14 23:24:31 +0000

    deploy: 69c61f23c0de67835a2eaac689b218429173428b

---
+++ preview/assets/BoxButton-gmxNnOgN.js
@@ -0,0 +1,14 @@
+import{r as n,v as d,j as l,w as p,o as f,x as m,k as b,c as r}from"./index-BSNveA1x.js";const v=({title:a,titleId:e,...s})=>n.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 32 32",fill:"none","aria-labelledby":e,...s},a?n.createElement("title",{id:e},a):null,n.createElement("path",{stroke:"currentColor",strokeLinecap:"round",strokeLinejoin:"round",d:"M8 16h8m0 0h8m-8 0V8m0 8v8"})),w=d(`
+    group inline-flex items-center justify-between w-full rounded-lg button-text font-button
+    transition-all outline-none cursor-pointer
+    focus-visible:ring-2 focus-visible:ring-yellow-accent-dark focus-visible:ring-offset-2
+    disabled:pointer-events-none disabled:opacity-50
+  `,{variants:{variant:{default:`
+          bg-white ring-1 ring-inset ring-yellow-accent-light text-black hover:ring-yellow-light
+          active:ring-2 active:ring-yellow-accent-dark
+        `,filled:"bg-yellow-accent-light text-black hover:bg-yellow-light active:bg-yellow-accent-dark"},size:{default:"h-[92px] p-5",small:"h-[72px] p-4"}},defaultVariants:{variant:"default",size:"default"}}),h={users:b,special_projects:m,box:f};function j({className:a,variant:e="default",size:s="default",asChild:i=!1,icon:t,smallIcon:c=!1,children:u,...x}){const g=i?p:"button",o=t?h[t]:null;return l.jsxs(g,{"data-slot":"button","data-variant":e,"data-size":s,className:r(w({variant:e,size:s,className:a})),...x,children:[l.jsxs("div",{className:"flex items-center gap-[7px]",children:[o&&l.jsx(o,{className:`
+              ${t==="box"&&"w-[37px] text-text-black-natural -ml-[7px]"}
+              ${t==="special_projects"&&(c?"w-[31px]":"w-[42px] -ml-[10px] -mr-[5px]")}
+              ${t==="users"&&"w-[42px] -ml-[10px] -mr-[5px]"}
+              shrink-0
+            `}),u]}),l.jsx("div",{className:r("flex size-12 shrink-0 items-center justify-center rounded-lg transition-all",e==="filled"?"bg-white":"bg-yellow-accent-light group-hover:bg-yellow-light group-active:bg-yellow-accent-dark"),children:l.jsx(v,{className:"size-8"})})]})}export{j as B,v as S};
