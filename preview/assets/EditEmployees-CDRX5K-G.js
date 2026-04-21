ab550a7191bdd3014ee6d0dd1872ab268adcdfb1
Gittenhuben <Gittenhuben@users.noreply.github.com>
2026-04-21 08:11:07 +0000

    deploy: d23c007c9537452e4a014ca08aa11e43caf9b6ba

---
+++ preview/assets/EditEmployees-CDRX5K-G.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Be8Rpw2V.js";const n=()=>e.jsxs(e.Fragment,{children:[e.jsx("h1",{children:"Редактировать сотрудника"}),e.jsx("p",{children:"Страница в разработке..."})]}),s=n;export{s as Component};
